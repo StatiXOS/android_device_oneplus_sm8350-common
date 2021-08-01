46ba459ad760546e21edbc06d53b64a9964ca212
Anay Wadhera <awadhera@berkeley.edu>
2021-07-31 17:14:16 -0700

    sm8350-common: add extra gestures and more accurately represent gestures
    Change-Id: I40d11849bc0ac16f3ce4a4aa5d091b4436e7e2c1

--- touch/TouchscreenGesture.cpp
+++ touch/TouchscreenGesture.cpp
@@ -26,38 +26,21 @@ namespace touch {
 namespace V1_0 {
 namespace implementation {
 
-/*
-#define KEY_F5                  63
-#define KEY_F6                  64
-#define KEY_F7                  65
-#define KEY_F8                  66
-#define KEY_F9                  67
-#define KEY_GESTURE_W               246
-#define KEY_GESTURE_M               247
-#define KEY_GESTURE_S               248
-#define KEY_DOUBLE_TAP              KEY_WAKEUP
-#define KEY_GESTURE_CIRCLE          250
-#define KEY_GESTURE_TWO_SWIPE       251
-#define KEY_GESTURE_UP_ARROW        252
-#define KEY_GESTURE_LEFT_ARROW      253
-#define KEY_GESTURE_RIGHT_ARROW     254
-#define KEY_GESTURE_DOWN_ARROW      255
-#define KEY_GESTURE_SWIPE_LEFT      KEY_F5
-#define KEY_GESTURE_SWIPE_DOWN      KEY_F6
-#define KEY_GESTURE_SWIPE_RIGHT     KEY_F7
-#define KEY_GESTURE_SWIPE_UP        KEY_F8
-#define KEY_GESTURE_SINGLE_TAP      KEY_F9
-*/
 const std::map<int32_t, TouchscreenGesture::GestureInfo> TouchscreenGesture::kGestureInfoMap = {
     {0, {KEY_GESTURE_TWO_SWIPE, "Two fingers down swipe", "/proc/touchpanel/double_swipe_enable"}},
-    {1, {KEY_GESTURE_DOWN_ARROW, "Down arrow", "/proc/touchpanel/down_arrow_enable"}},
-    {2, {KEY_GESTURE_LEFT_ARROW, "Left arrow", "/proc/touchpanel/left_arrow_enable"}},
-    {3, {KEY_GESTURE_RIGHT_ARROW, "Right arrow", "/proc/touchpanel/right_arrow_enable"}},
+    {1, {KEY_GESTURE_DOWN_ARROW, "Down V", "/proc/touchpanel/down_arrow_enable"}},
+    {2, {KEY_GESTURE_LEFT_ARROW, "> Symbol", "/proc/touchpanel/left_arrow_enable"}},
+    {3, {KEY_GESTURE_RIGHT_ARROW, "< Symbol", "/proc/touchpanel/right_arrow_enable"}},
     {4, {KEY_GESTURE_M, "Letter M", "/proc/touchpanel/letter_m_enable"}},
     {5, {KEY_GESTURE_CIRCLE, "Letter O", "/proc/touchpanel/letter_o_enable"}},
     {6, {KEY_GESTURE_S, "Letter S", "/proc/touchpanel/letter_s_enable"}},
     {7, {KEY_GESTURE_W, "Letter W", "/proc/touchpanel/letter_w_enable"}},
     {8, {KEY_GESTURE_SINGLE_TAP, "Single Tap", "/proc/touchpanel/single_tap_enable"}},
+    {9, {KEY_GESTURE_UP_ARROW, "Up V", "/proc/touchpanel/up_arrow_enable"}},
+    {10,{KEY_GESTURE_SWIPE_UP, "Up to down swipe", "/proc/touchpanel/up_swipe_enable"}},
+    {11,{KEY_GESTURE_SWIPE_DOWN, "Down to up swipe", "/proc/touchpanel/down_swipe_enable"}},
+    {12,{KEY_GESTURE_SWIPE_LEFT, "Left to right swipe", "/proc/touchpanel/left_swipe_enable"}},
+    {13,{KEY_GESTURE_SWIPE_RIGHT, "Right to left swipe", "/proc/touchpanel/right_swipe_enable"}},
 };
 
 Return<void> TouchscreenGesture::getSupportedGestures(getSupportedGestures_cb resultCb) {
