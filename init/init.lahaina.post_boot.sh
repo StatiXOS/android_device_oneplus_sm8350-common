65b89c7d8ae5600b83e4d81d871818a0dd5d95a0
Kyle Lin <kylelin@google.com>
2023-03-05 00:06:59 +0600

    sm8350-common: init: fine tune the read_ahead size
    fine tune the read_ahead size for runtime
    
    Change-Id: If7929c51222e325a019532ce0d16045a52778165
    Signed-off-by: Kyle Lin <kylelin@google.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- init/init.lahaina.post_boot.sh
+++ init/init.lahaina.post_boot.sh
@@ -5,3 +5,17 @@
 
 # Custom tuning for Lahaina SoC
 
+# Runtime fs tuning
+write /sys/block/sda/queue/read_ahead_kb 128
+write /sys/block/sda/queue/nr_requests 128
+write /sys/block/sda/queue/iostats 1
+write /sys/block/dm-0/queue/read_ahead_kb 128
+write /sys/block/dm-1/queue/read_ahead_kb 128
+write /sys/block/dm-2/queue/read_ahead_kb 128
+write /sys/block/dm-3/queue/read_ahead_kb 128
+write /sys/block/dm-4/queue/read_ahead_kb 128
+write /sys/block/dm-5/queue/read_ahead_kb 128
+write /sys/block/dm-6/queue/read_ahead_kb 128
+write /sys/block/dm-7/queue/read_ahead_kb 128
+write /sys/block/dm-8/queue/read_ahead_kb 128
+write /sys/block/dm-9/queue/read_ahead_kb 128
